7b3d969a243ca774c19454399c8c0e3b781476aa
Bug Fest <52962234+bugfest@users.noreply.github.com>
2022-07-18 10:48:27 +0200

    SIGTERM kill to end the build process

--- cli.js
+++ cli.js
@@ -154,6 +154,7 @@ module.exports = async () => {
         if (options.GENERATE_WEBSITE && !program.watch) {
             console.log(chalk.gray('\nto view the generated website run'));
             console.log(`> c4builder site`);
+            process.kill(process.pid, 'SIGTERM');
         }
     }
 };
